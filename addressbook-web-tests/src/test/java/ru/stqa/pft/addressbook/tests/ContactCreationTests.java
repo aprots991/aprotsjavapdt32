1983af7f5a1298f6cf37174535095777450fbbf7
aprots991 <anton113@yandex.ru>
2016-09-04 21:08:06 +0400

    dz8.1
    NewContact page verification addee ant method moved to Navigation helper

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -7,7 +7,7 @@ public class ContactCreationTests extends TestBase {
 
   @Test
   public void testContactCreation() {
-    app.getContactHelper().gotoAddNewConactPage();
+    app.getNavigationHelper().gotoAddNewContactPage();
     app.getContactHelper().fillContactForm(new ContactData("John", null, "Brown", "jho", "title", "test1", "company", "address", "4955555555",
             "901900222", "78985469521", "8-985-254879", "mymail@myc.ru", "mymail2@myc.ru",
             "mymail3@myc.ru", "http://myc.ru", "Moscow, Russia Leningradsky st. 45 b. 4 ap. 449", "433",
