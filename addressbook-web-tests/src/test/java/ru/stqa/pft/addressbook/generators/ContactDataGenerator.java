ad2bd0c618497e9d479f3495a1ed1e8d1631b79f
aprots991 <anton113@yandex.ru>
2016-10-09 19:52:41 +0400

    dz14_15_fix
    added try to generators and providrers

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
@@ -54,9 +54,9 @@ public class ContactDataGenerator {
   private void saveAsJson(List<ContactData> contacts, File file) throws IOException {
     Gson gson = new GsonBuilder().setPrettyPrinting().excludeFieldsWithoutExposeAnnotation().create();
     String json = gson.toJson(contacts);
-    Writer writer = new FileWriter(file);
-    writer.write(json);
-    writer.close();
+    try (Writer writer = new FileWriter(file)) {
+      writer.write(json);
+    }
   }
 
   private void saveAsXml(List<ContactData> contacts, File file) throws IOException {
@@ -64,18 +64,18 @@ public class ContactDataGenerator {
     xstream.alias("contact", ContactData.class);
     xstream.processAnnotations(ContactData.class);
     String xml = xstream.toXML(contacts);
-    Writer writer = new FileWriter(file);
-    writer.write(xml);
-    writer.close();
+    try (Writer writer = new FileWriter(file)) {
+      writer.write(xml);
+    }
   }
 
   private void saveAsCsv(List<ContactData> contacts, File file) throws IOException {
-    Writer writer = new FileWriter(file);
-    for (ContactData contact : contacts) {
-      writer.write(String.format("%s; %s; %s; %s; %s \n", contact.getFirstName(), contact.getLastName(), contact.getAddress(),
-              contact.getHomePhone(), contact.getEmail()));
+    try (Writer writer = new FileWriter(file)) {
+      for (ContactData contact : contacts) {
+        writer.write(String.format("%s; %s; %s; %s; %s \n", contact.getFirstName(), contact.getLastName(), contact.getAddress(),
+                contact.getHomePhone(), contact.getEmail()));
+      }
     }
-    writer.close();
   }
 
   private List<ContactData> generateContacts(int count) {
